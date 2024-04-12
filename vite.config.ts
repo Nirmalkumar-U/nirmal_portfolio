860bd886e1c81477be4fd3cc8084c6f4fd22f94b
Nirmalkumar-U <unirmal56@gmail.com>
2024-04-12 13:19:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { ghPages } from 'vite-plugin-gh-pages'; 
 
 export default defineConfig({
-  base: '/Portfolio/',
+  base: './',
   plugins: [
     react(),
     ghPages()
