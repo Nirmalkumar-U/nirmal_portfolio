b949deeda53249ddba21fa78986f285749546c5b
unirmal56 <unirmal56@gmail.com>
2024-04-18 05:58:14 +0000

    img changes

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -11,15 +11,22 @@ function Project(datas:any){
                 <div className='project'>
                     <h2 data-aos="fade-right" data-aos-duration="1500" data-aos-delay="500">Projects</h2>
                     <div className='project-cards'>
-                        {data.project.map((project: any, i: any) => (
-                            <div className='project-card' data-aos="fade-left" data-aos-duration="1500" data-aos-delay="2500" key={i}>
-                                <img src={project.img} height={300} width={500}  alt="project-img" />
+                        {/* {data.project.map((project: any, i: any) => ( */}
+                            <div className='project-card' data-aos="fade-left" data-aos-duration="1500" data-aos-delay="2500">
+                                <img src="./src/assets/smarttools.png" height={300} width={500}  alt="project-img" />
                                 <div>
-                                    <h4>{project.name}</h4>
-                                    <p>{project.description}</p>
+                                    <h4>{data.project[0].name}</h4>
+                                    <p>{data.project[0].description}</p>
                                 </div>
                             </div>
-                        ))}
+                            <div className='project-card' data-aos="fade-left" data-aos-duration="1500" data-aos-delay="2500">
+                                <img src="./src/assets/hmids_logo.png" height={300} width={500}  alt="project-img" />
+                                <div>
+                                    <h4>{data.project[1].name}</h4>
+                                    <p>{data.project[1].description}</p>
+                                </div>
+                            </div>
+                        {/* ))} */}
 
                     </div>
                 </div>
